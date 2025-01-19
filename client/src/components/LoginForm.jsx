e7178a3b59241ef7d229d9e5a412c5a444b113bb
Mohit Mehto <imohitmehto@gmail.com>
2025-01-19 23:42:38 +0530

    change urls

--- client/src/components/LoginForm.jsx
+++ client/src/components/LoginForm.jsx
@@ -20,7 +20,7 @@ const LoginForm = () => {
   const handleGoogleSuccess = async (credentialResponse) => {
     try {
       const tokenId = credentialResponse.credential;
-      const response = await fetch("http://localhost:5000/api/auth/google", {
+      const response = await fetch("https://ecommerce-backend-olive-ten.vercel.app//api/auth/google", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
@@ -59,7 +59,7 @@ const LoginForm = () => {
   const handleEmailPasswordLogin = async (e) => {
     e.preventDefault();
     try {
-      const response = await fetch("http://localhost:5000/api/auth/login", {
+      const response = await fetch("https://ecommerce-backend-olive-ten.vercel.app/api/auth/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
--- client/src/services/authService.js
+++ client/src/services/authService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api/auth';
+const API_URL = 'https://ecommerce-backend-olive-ten.vercel.app/api/auth';
 
 export const signup = (data) => axios.post(`${API_URL}/signup`, data);
 export const login = (data) => axios.post(`${API_URL}/login`, data);
